0db285dc307d6db9eeba772d0e315eed52790cd7
ramzi12rtx <ughggvbg@gmail.com>
2025-07-24 23:11:26 +0100

    Update video_generator.py

--- src/video_generator.py
+++ src/video_generator.py
@@ -1,18 +1,16 @@
-from moviepy.editor import *
+from moviepy.editor import ImageClip, AudioFileClip, CompositeVideoClip
 import os
 from datetime import datetime
 
-def create_video(audio_path):
-    if not os.path.exists("background.jpg"):
-        raise Exception("❌ الصورة background.jpg غير موجودة.")
+def create_video(image_path, audio_path, output_path=None):
+    if not output_path:
+        os.makedirs("output", exist_ok=True)
+        output_path = f"output/video_{datetime.now().strftime('%Y%m%d%H%M%S')}.mp4"
 
     try:
-        image_clip = ImageClip("background.jpg").set_duration(AudioFileClip(audio_path).duration)
         audio_clip = AudioFileClip(audio_path)
-        video = image_clip.set_audio(audio_clip)
-
-        output_path = f"output/video_{datetime.now().strftime('%Y%m%d%H%M%S')}.mp4"
-        os.makedirs("output", exist_ok=True)
+        image_clip = ImageClip(image_path).set_duration(audio_clip.duration).resize(height=720)
+        video = CompositeVideoClip([image_clip.set_audio(audio_clip)])
         video.write_videofile(output_path, fps=24)
         print(f"✅ Video saved at: {output_path}")
         return output_path
